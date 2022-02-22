80fdd3905bde3b31e6f1a49260752c3a3982dcdd
Casterminion <arijusblaviesciunas@gmail.com>
2022-02-22 14:00:12 +0200

    commit

--- main.py
+++ main.py
@@ -12,3 +12,56 @@ def sum(a, b):
 	print(f'{a} + {b} = {a + b }')
 sum( a , b)
 print('-'*20)
+
+
+#Tito pamoka
+
+a =10
+b = int(input ('B: '))
+
+
+if a>b:
+	print('A > B')
+else:
+	print('A < B')
+
+if a>b:
+	print('A > B')
+elif a == b:
+	print('A < B')
+else:
+	print('A < B')
+
+	
+#If with bools	
+c = False
+
+if not c:
+	print('C is False')
+
+#If negation
+d = True
+
+if  d:
+	print('C is True')
+
+#If and
+
+a1 = 2
+a2 = True
+
+if a1 < 5 and a2:
+	print('Trigger')
+
+# < > == !== >=  <=
+
+b1 = 5
+b2 = 10
+if b1 > 1 or b2 > 5:
+	print('Trigger2')
+
+
+#naudojimas boolean
+# name -str
+#autorius -str
+#Rented - bool

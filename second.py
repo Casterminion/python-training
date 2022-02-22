d1e6c7f3b1399798d85fa0d0dc5333863883602a
Casterminion <arijusblaviesciunas@gmail.com>
2022-02-22 14:35:47 +0200

    commit

---
+++ second.py
@@ -0,0 +1,37 @@
+class Computer:
+    def __init__(self, cpu, gpu, ram_amount):
+        self.cpu = cpu
+        self.gpu = gpu
+        self.ram_amount = ram_amount
+
+def info(self):
+    print(f'CPU: {self.cpu}')
+    print(f'GPU: {self.gpu}')
+    print(f'RAM: {self.ram_amount}')
+
+
+class Computer:
+    def __init__(self, name, age,):
+        self.name = name
+        self.age = age
+        
+
+def info(self):
+    print(f'I\'m {self.name}')
+  
+
+
+
+
+#new object
+my_pc = Computer('potato', 'Peciulis', '69GB')
+
+#using function in calss
+my_pc.info()
+#changing properties
+my_pc.change_cpu('sollapanel potato')
+my_pc.info()
+
+my_pc.cpu ='AA'
+
+my_pc.info()

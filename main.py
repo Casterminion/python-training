1bd8b13bbe628277c3c55bada9a58da9d016c2b5
Casterminion <arijusblaviesciunas@gmail.com>
2022-02-22 11:00:40 +0200

    commit

---
+++ main.py
@@ -0,0 +1,13 @@
+
+name = input("Write your name: ")
+age = input("What's your age? ")
+print(f'Hello, {name}')
+print(f'Your age is {age}')
+
+print('-'*20)
+print('Write down two numbers that you would like to sum ')
+a = int(input('Type first number = '))
+b = int(input('Type second number = '))
+def sum(a, b):
+	print(f'{a} + {b} = {a + b}')
+sum(a ,b)
